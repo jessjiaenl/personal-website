1454c021b9ac63d607c7d16a080ecee6f8f74544
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2024-06-06 15:17:55 +0800

    prog(games): can now navigate between home and games page

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,10 +1,41 @@
+import { motion } from "framer-motion";
+import Floor from '../objects/Floor';
+
 function Games() {
   return (
-    <div>
-      <p>
-        My Game Projects
-      </p>
-    </div>
+    <>
+      <Floor/>
+      <a href="/">
+        <motion.img src={process.env.PUBLIC_URL + "/assets/exit.svg"}
+          style={{height:"10vh", position:"absolute", top: "5vh", left:"10vh"}}
+          whileHover={{scale:1.1}}
+          whileTap={{scale:1}}/>
+      </a>
+      <a href="https://github.com/jessjiaenl/snowboarding" target="_blank">
+        <motion.img src={process.env.PUBLIC_URL + "/assets/snowboarding.svg"}
+                    style={{height:"20vh", position:"absolute", top:"20vh", left:"25vw"}}
+                    whileHover={{scale:1.1}}
+                    whileTap={{scale:1}}/>
+      </a>
+      <a href="https://www.youtube.com/watch?v=o_sosPxMxOk" target="_blank">
+        <motion.img src={process.env.PUBLIC_URL + "/assets/WBH.svg"}
+                    style={{height:"20vh", position:"absolute", top:"20vh", left:"55vw"}}
+                    whileHover={{scale:1.1}}
+                    whileTap={{scale:1}}/>
+      </a>
+      <a href="https://www.youtube.com/watch?v=gQKX-AO6mgU" target="_blank">
+        <motion.img src={process.env.PUBLIC_URL + "/assets/haunted.svg"}
+                    style={{height:"20vh", position:"absolute", top:"45vh", left:"25vw"}}
+                    whileHover={{scale:1.1}}
+                    whileTap={{scale:1}}/>
+      </a>
+      <a href="https://www.youtube.com/watch?v=kGvr8PjbJAI" target="_blank">
+        <motion.img src={process.env.PUBLIC_URL + "/assets/LBL.svg"}
+                    style={{height:"20vh", position:"absolute", top:"45vh", left:"55vw"}}
+                    whileHover={{scale:1.1}}
+                    whileTap={{scale:1}}/>
+      </a>
+    </>
   );
 }
 
