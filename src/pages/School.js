184122f5e3399a034d4bf3fbb2c5f6493d01e870
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2024-06-09 10:12:30 +0800

    fix(rename): bench pillar rename issue resolved

--- src/pages/School.js
+++ src/pages/School.js
@@ -5,7 +5,8 @@ function School() {
   return (
     <>
       <Floor/>
-      <table>
+      <table style={{position:"absolute", top:"5vh"}}>
+        <caption>Carnegie Mellon University</caption>
         <thead>
           <th scope="col">Course</th>
           <th scope="col">Notes</th>
@@ -17,14 +18,44 @@ function School() {
             <td>Computer Systems</td>
             <td>A</td>
           </tr>
-
           <tr>
             <th scope="row">15210</th>
             <td>Parallel Data Structure & Algorithms</td>
             <td>A</td>
           </tr>
+          <tr>
+            <th scope="row">15150</th>
+            <td>Functional Programming</td>
+            <td>A</td>
+          </tr>
+          <tr>
+            <th scope="row">05318</th>
+            <td>Human-AI Interaction</td>
+            <td>A</td>
+          </tr>
+          <tr>
+            <th scope="row">10315</th>
+            <td>Machine Learning</td>
+            <td>A</td>
+          </tr>
+          <tr>
+            <th scope="row">15281</th>
+            <td>Artificial Intelligence</td>
+            <td>A</td>
+          </tr>
+          <tr>
+            <th scope="row">15122</th>
+            <td>Imperative Computation (Data Structure & Algorithms)</td>
+            <td>A</td>
+          </tr>
+          <tr>
+            <th scope="row">15112</th>
+            <td>Fundamentals of Programming</td>
+            <td>A</td>
+          </tr>
         </tbody>
       </table>
+
     </>
   );
 }
