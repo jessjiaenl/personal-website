428e2550db03288bd0e029951a6ae20bf3c85a8c
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2024-06-14 23:46:29 +0800

    fix(fence, resume): fence zIndex, new resume, activate diploma

--- src/components/Diploma.js
+++ src/components/Diploma.js
@@ -8,13 +8,15 @@ function Diploma(props) {
   // go back with : href="/" onClick={handleBackClick}
 
   return (
-    <a> {/* href="/school" */}
-      <motion.img src={process.env.PUBLIC_URL + "/assets/diploma.svg"}
-                  style={{height:"30vh", position:"absolute", top:"15vh", left:props.left}}
-                  // whileHover={{scale:1.1}}
-                  // whileTap={{scale:1}}
-                  />
-    </a>
+    <>
+      <a href="/school" > {/* href="/school" */}
+        <motion.img src={process.env.PUBLIC_URL + "/assets/diploma.svg"}
+                    style={{height:"30vh", position:"absolute", top:"15vh", left:props.left}}
+                    whileHover={{scale:1.1}}
+                    whileTap={{scale:1}}
+                    />
+      </a>
+    </>
   );
 }
 
--- src/components/Fence.js
+++ src/components/Fence.js
@@ -2,7 +2,7 @@ function Fence(props) {
   return (
     <div>
       <img src={process.env.PUBLIC_URL + "/assets/fence.svg"}
-        style={{height:"40vh", position:"absolute", top:"55vh", left:props.left}}/>
+        style={{height:"40vh", position:"absolute", top:"55vh", left:props.left, zIndex:10}}/>
     </div>
   );
 }
