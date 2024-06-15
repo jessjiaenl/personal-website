46b0dfcb308a2d6dcae3406b9d239217f480ebd1
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2024-06-15 15:37:28 +0800

    fix(hello, about): moved tags to hello, added descriptions in about

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,17 +1,23 @@
 function AboutMe(props) {
   return (
     <div>
-      <h1 style={{position:"absolute", top: "20vh", left:props.left}}>
+      <h1 style={{position:"absolute", top: "18vh", left:props.left}}>
         About me
       </h1>
       <h2 style={{position:"absolute", top: "35vh", left:props.left}}>
-        <b>Languages:</b> Python, C/C++, SML, C#, JavaScript, HTML, CSS<br/><br/>
+      I applied machine learning to large-scale real-world data <br/>
+      in computer vision and retail from analyzing and preprocessing <br/>
+      the data, building and optimizing ML models, to deploying on<br/>
+       GCP and Edge devices. <br/>
+       Beside industrial experiences I also built some games.<br/><br/>
+
+        <b>Languages:</b> Python, C/C++, SML, C#, JavaScript, HTML, CSS<br/>
         <b>Tools:</b> Git, TensorFlow, PyTorch, OpenCV, GCP, Unity, React, <br/>
         NumPy, Pandas, Scikit-learn, Linux<br/><br/>
-        <b> 
+        {/* <b> 
           #AnalyticalSkills #FastLearner #Empath #Organizational <br/>
-          #Interdisciplinary #Calm
-        </b>
+          #Interdisciplinary #Calm <br/>
+        </b> */}
 
       </h2>
     </div>
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -7,9 +7,21 @@ function Hello(props) {
       <h2 style={{position:"absolute", top: "38vh", left:props.left}}>
       Passionate about learning and interdisciplinary work, <br/>
       Jessica studied <b>Mathematics</b>, <b>Computer Science</b>, and <br/>
-      <b>Game Design</b> at Carnegie Mellon University and decided to <br/>
-      specialize in Computer Science at Cornell (Tech).
+      <b>Game Design</b> at Carnegie Mellon University and is currently <br/>
+      specializing in <b>Computer Science</b> at Cornell (Tech). <br/><br/>
+
+      #AnalyticalSkills #FastLearner #Empath #Organizational <br/>
+      #Interdisciplinary #Teamwork <br/>
+      
+      {/* She has applied machine learning to large-scale real-world <br/>
+      data in computer vision and retail from analyzing and <br/>
+      preprocessing the data, building and optimizing ML models,<br/>
+      to deploying on GCP and Edge devices. */}
       </h2>
+      {/* <h2 style={{position:"absolute", top: "58vh", left:props.left, fontSize:"2vh", fontWeight:500}}>
+        #AnalyticalSkills #FastLearner #Empath #Organizational <br/>
+        #Interdisciplinary #Calm <br/>
+      </h2> */}
     </div>
   );
 }
