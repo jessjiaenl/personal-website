ed7bb9ad1b4ae7c3d9ee0006a87a56b787912a79
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2024-06-15 15:10:29 +0800

    prog(aboutme): added more content about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,10 +5,14 @@ function AboutMe(props) {
         About me
       </h1>
       <h2 style={{position:"absolute", top: "35vh", left:props.left}}>
-        <b>Languages:</b> Python, C/C++, SML, C#, JavaScript, HTML, CSS<br/>
+        <b>Languages:</b> Python, C/C++, SML, C#, JavaScript, HTML, CSS<br/><br/>
         <b>Tools:</b> Git, TensorFlow, PyTorch, OpenCV, GCP, Unity, React, <br/>
-        NumPy, Pandas, Scikit-learn <br/><br/>
-        <b> #AnalyticalSkills #FastLearner #Empath #Organizational</b>
+        NumPy, Pandas, Scikit-learn, Linux<br/><br/>
+        <b> 
+          #AnalyticalSkills #FastLearner #Empath #Organizational <br/>
+          #Interdisciplinary #Calm
+        </b>
+
       </h2>
     </div>
   );
