2ebc02fefb9b82acb4e1fa52a92a7545d40c011f
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2024-06-18 09:38:36 +0800

    prog(horror): added horror self portrait on hover but disabled it

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
 function Me(props) {
+  const horrorPortraitUrl = process.env.PUBLIC_URL + "/assets/horrorSelfPortrait.gif";
+  const normalPortraitUrl = process.env.PUBLIC_URL + "/assets/pixelatedMe.gif";
+  const [currentPortraitUrl, setCurrentPortraitUrl] = useState(normalPortraitUrl);
+
+  const switchToHorror = () => {
+    setCurrentPortraitUrl(horrorPortraitUrl);
+  }
+  const switchToNormal = () => {
+    setCurrentPortraitUrl(normalPortraitUrl);
+  }
+
   return (
     <div style={{position:"absolute", top:"20vh", left:props.left}}>
       <img src={process.env.PUBLIC_URL + "/assets/frames/emptyFrame.svg"}
         style={{height:"45vh", position:"absolute"}}/>
-      <img src={process.env.PUBLIC_URL + "/assets/pixelatedMe.gif"} 
+      <img src={currentPortraitUrl} 
+        // onMouseEnter={switchToHorror}
+        // onMouseLeave={switchToNormal}
         style={{height:"45vh", position:"absolute", top:"-5vh", left:`calc(${props.left} - 26.5vh)`}}/>
     </div>
   );
